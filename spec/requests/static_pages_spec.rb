4c84b5ea910639638eb5670b2d3427964eea693d
ChanellJ <cjackson@chicagotechacademy.org>
2014-04-17 13:20:12 -0500

    Part 11 20:46

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -17,7 +17,7 @@ describe "StaticPages" do
   describe "About Us page" do
     before {visit about_path}
     it { should have_selector("h1", text: 'About Us') }
-    it { should have_selector("title", text: 'About Page') }
+    it { should have_selector("title", text: 'About Us') }
   end  
   describe "Contact page" do
      before {visit contact_path}
@@ -25,5 +25,16 @@ describe "StaticPages" do
     it { should have_selector("h1", text: 'Contact') }
     it { should have_selector("title", text: 'Contact Page') }
   end
-
+  it "should have correct links" do 
+    visit root_path
+    click_link "About"
+    page.should have_selector "title", text: "About Us"
+    click_link "Help" 
+    page.should have_selector "title", text: "Help"
+    click_link "Contact"
+    page.should have_selector "title", text: "Contact"
+    click_link "Home"
+    click_link "Sign up now!"
+    page.should have_selector "title", text: "Sign up"
+  end
 end
